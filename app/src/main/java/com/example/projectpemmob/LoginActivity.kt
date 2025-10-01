a73602bb91b3c7571c1eca1d2c2c7f8bb95d9cf1
AthayaRaihan <athayaraihanannafi@gmail.com>
2025-10-01 22:09:54 +0700

    commit 1

--- app/src/main/java/com/example/projectpemmob/LoginActivity.kt
+++ app/src/main/java/com/example/projectpemmob/LoginActivity.kt
@@ -2,25 +2,25 @@ package com.example.projectpemmob
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
-        val btnMasuk = findViewById<MaterialButton>(R.id.btnMasuk)
-        val btnJelajah = findViewById<MaterialButton>(R.id.btnJelajah)
+        // Ambil view dari XML
+        val tvRegister = findViewById<TextView>(R.id.tvRegister)
 
-        btnMasuk.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
+        // Arahkan ke RegisterActivity kalau diklik
+        tvRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
--- app/src/main/java/com/example/projectpemmob/RegisterActivity.kt
+++ app/src/main/java/com/example/projectpemmob/RegisterActivity.kt
@@ -2,25 +2,25 @@ package com.example.projectpemmob
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
 
-        val btnMasuk = findViewById<MaterialButton>(R.id.btnMasuk)
-        val btnJelajah = findViewById<MaterialButton>(R.id.btnJelajah)
+        val tvLogin = findViewById<TextView>(R.id.tvLogin)
 
-        btnMasuk.setOnClickListener {
+        // Arahkan ke LoginActivity kalau diklik
+        tvLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+            finish() // optional, supaya user tidak bisa kembali ke register dengan tombol back
         }
-
     }
 }
