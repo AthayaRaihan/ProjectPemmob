19fd01e69d48bfcb7e5a05a171f08edea56b4395
AthayaRaihan <athayaraihanannafi@gmail.com>
2025-10-03 22:58:10 +0700

    Homepage sudah ada nama user

--- app/src/main/java/com/example/projectpemmob/HomepageActivity.kt
+++ app/src/main/java/com/example/projectpemmob/HomepageActivity.kt
@@ -1,11 +1,23 @@
 package com.example.projectpemmob
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.example.projectpemmob.R
 
 class HomepageActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_home)
+
+        val tvGreetingName = findViewById<TextView>(R.id.tvGreetingName)
+
+        // Ambil user yang login
+        val user = FirebaseAuth.getInstance().currentUser
+
+        val displayName = user?.displayName ?: "User" // fallback kalau null
+        tvGreetingName.text = "Hai $displayName,"
     }
 }
